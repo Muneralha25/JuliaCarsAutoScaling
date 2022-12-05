9502ab1a6ca3ab57afa64cc5c7144b14aeb06faf
Muneralha25 <109544121+Muneralha25@users.noreply.github.com>
2022-12-05 13:45:33 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -55,3 +55,9 @@ variable "load_balancer_max_band" {
   description = "Load Balancer Max Band"
   default     = "10"
 }
+
+variable "minimal_size" {
+  description = "tamanho inicial do Scalling Web"
+  default     = "1"
+}
+
