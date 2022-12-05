513e1474138bfc345a6b50ebc3d479a5d6bf5a16
Muneralha25 <109544121+Muneralha25@users.noreply.github.com>
2022-12-05 12:59:37 -0300

    Update instance_autoscaling.tf

--- instance_autoscaling.tf
+++ instance_autoscaling.tf
@@ -8,7 +8,7 @@ resource "oci_autoscaling_auto_scaling_configuration" "JuliaCarsThresholdAutoSca
     policies {
         display_name = "JuliaCarsThresholdAutoScalingConfigurationPolicies"
         capacity {
-            initial = "1"
+            initial = "var.InitialcalingSize"
             max = "2"
             min = "1"
         }
